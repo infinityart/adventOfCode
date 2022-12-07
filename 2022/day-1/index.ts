28c2e05f795f39e619bafd73c3c61c7b2eaefb66
Jonty Sponselee <jsponselee97@gmail.com>
2022-12-08 00:38:06 +0100

    Add 2022 day-1 part-2

--- 2022/day-1/index.ts
+++ 2022/day-1/index.ts
@@ -1,15 +1,25 @@
 import {emptyLine, newLine} from "../../util/regexes.ts";
 
-export function part1(input: string) {
+function getCaloriesPerElf(input: string) {
     const countTotalCalories = (a, b) => parseInt(a) + parseInt(b);
 
     const caloriePerElfList = input.split(emptyLine);
 
-    const caloriesPerElf = caloriePerElfList.map(caloriePerElf => {
+    return caloriePerElfList.map(caloriePerElf => {
         return caloriePerElf
             .split(newLine)
             .reduce(countTotalCalories, 0)
     });
+}
 
-    return Math.max(...caloriesPerElf)
+export function part1(input: string) {
+    return Math.max(...getCaloriesPerElf(input))
+}
+
+export function part2(input: string) {
+    const sortDescending = (a: number, b: number) => b - a;
+
+    const [highest, secondHighest, thirdHighest] = getCaloriesPerElf(input).sort(sortDescending);
+
+    return highest + secondHighest + thirdHighest;
 }
