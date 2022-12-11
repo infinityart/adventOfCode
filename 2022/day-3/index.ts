f80e6345f07a91ffdece78b4e679ac46316903d1
Jonty Sponselee <jsponselee97@gmail.com>
2022-12-12 00:22:07 +0100

    Add 2022 day-3 part-1

--- 2022/day-3/index.ts
+++ 2022/day-3/index.ts
@@ -1,3 +1,33 @@
-export function part1(input: string) {
+import {newLine} from "../../util/regexes.ts";
 
+export function part1(input: string) {
+    const sumPriority = (a, b) => a + b;
+
+    function findSharedItem(rucksack: string) {
+        const middleIdx = rucksack.length / 2;
+        const firstCompartment = rucksack.slice(0, middleIdx);
+        const secondCompartment = rucksack.slice(middleIdx, -1);
+
+        let sharedItem: [string];
+        for (const item of firstCompartment) {
+            sharedItem = secondCompartment.match(new RegExp(item));
+
+            if (sharedItem !== null) break;
+        }
+
+        return sharedItem[0]
+    }
+
+    function convertToPriority(sharedItem: string) {
+        const isUppercase = sharedItem === sharedItem.toUpperCase();
+        const basePriority = (isUppercase ? ("A".charCodeAt(0) - 26) : "a".charCodeAt(0)) - 1;
+
+        return sharedItem.charCodeAt(0) - basePriority;
+    }
+
+    const rucksacks = input.split(newLine);
+
+    return rucksacks
+        .map(rucksack => convertToPriority(findSharedItem(rucksack)))
+        .reduce(sumPriority, 0)
 }
