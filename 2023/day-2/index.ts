b4a7663905cd92ba56ee9ae51a896a3e19c329f4
Jonty Sponselee <jsponselee97@gmail.com>
2023-12-02 16:42:27 +0100

    2023 day-2 part-1

---
+++ 2023/day-2/index.ts
@@ -0,0 +1,29 @@
+import {newLine, space} from "../../util/regexes.ts";
+
+export function part1(input: string) {
+    const maxCubes = {
+        red: 12,
+        green: 13,
+        blue: 14
+    };
+
+    return input.split(newLine).reduce((gameIdSum, line) => {
+        const [game, combinedSets] = line.split(":");
+        const [, gameId] = game.split(space);
+        const sets = combinedSets.split(";");
+
+        const isPossible = sets.every(set => {
+            const cubes = set.trim().split(",");
+
+            return cubes.every(cube => {
+                const [cubeCount, cubeColour] = cube.trim().split(space);
+
+                return parseInt(cubeCount) <= maxCubes[cubeColour]
+            })
+        });
+
+        if (!isPossible) return gameIdSum;
+
+        return gameIdSum + parseInt(gameId)
+    }, 0);
+}
